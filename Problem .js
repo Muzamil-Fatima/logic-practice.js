bc911f418541724d40efb0dcb72534ea4c3b84a8
Muzamil <muzamilzubair987654@gmail.com>
2025-07-25 05:05:03 +0000

    slove problem

--- Problem .js
+++ Problem .js
@@ -1,37 +1,20 @@
-// Problem: Check if Two Strings Are Anagrams
-// console.log("Problem: Check if Two Strings Are Anagrams")
-// function StrAnagrams(str1, str2) {
-// const letter1 = str1.toLowerCase().split("")
-// const letter2 = str2.toLowerCase().split("")
+// Problem: Find the Second Largest Number in an Array
+function secondLargest(array) {
+    let max = -Infinity;
+    let secondMax = -Infinity;
 
-//     return {str1, str2}
-// }
-// console.log(StrAnagrams("listen", "silent"))
-// console.log(StrAnagrams("triangle", "integral"))
-// console.log(StrAnagrams("hello", "world"))
-
-// Problem: Find the Most Frequent Number
-console.log("Problem: Find the Most Frequent Number");
-
-function MostFrequent(array) {
-    let arrange = array.toSorted((a, b) => a - b);
-    let result = {};
-    let maxCount = 0;
-    let mostFrequentNum = arrange[0];
-
-    arrange.forEach(element => {
-        result[element] = (result[element] || 0) + 1;
-
-        if (result[element] > maxCount) {
-            maxCount = result[element];
-            mostFrequentNum = element;
+    array.forEach(num => {
+        if (num > max) {
+            secondMax = max;
+            max = num;
+        } else if (num > secondMax && num < max) {
+            secondMax = num;
         }
     });
 
-    return mostFrequentNum;
+    return secondMax === -Infinity ? null : secondMax;
 }
 
-console.log(MostFrequent([2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 67, 788, 899, 9, 9, 9]));
-console.log(MostFrequent([1, 3, 2, 3, 4, 3, 2, 2]));
-console.log(MostFrequent([5, 5, 6, 7, 7, 7, 5]));
-console.log(MostFrequent([10, 20, 30]));
+console.log(secondLargest([10, 5, 20, 20, 4])); 
+console.log(secondLargest([3, 3, 3]));         
+console.log(secondLargest([1, 2, 3, 4, 5]));   
--- Problems.js
+++ Problems.js
@@ -668,3 +668,66 @@ function MostFrequent(array) {
     }
 }
 console.log(MostFrequent([2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 67, 788, 899, 9, 9, 9]))
+// Problem: Find the Most Frequent Number
+console.log("Problem: Find the Most Frequent Number");
+
+function MostFrequent(array) {
+    let arrange = array.toSorted((a, b) => a - b);
+    let result = {};
+    let maxCount = 0;
+    let mostFrequentNum = arrange[0];
+
+    arrange.forEach(element => {
+        result[element] = (result[element] || 0) + 1;
+
+        if (result[element] > maxCount) {
+            maxCount = result[element];
+            mostFrequentNum = element;
+        }
+    });
+
+    return mostFrequentNum;
+}
+
+console.log(MostFrequent([2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 67, 788, 899, 9, 9, 9]));
+console.log(MostFrequent([1, 3, 2, 3, 4, 3, 2, 2]));
+console.log(MostFrequent([5, 5, 6, 7, 7, 7, 5]));
+console.log(MostFrequent([10, 20, 30]));
+// Problem: Most Frequent Character in a String
+console.log("Problem: Most Frequent Character in a String")
+
+function mostFrequentChar(str) {
+    let result = {}
+    let maxCount = 0
+    let maxChar = ""
+    let char = str.split("")
+    char.forEach(letter => {
+        if (!result[letter]) {
+            result[letter] = 1
+        }
+        else {
+            result[letter]++
+        }
+        if (result[letter] > maxCount) {
+            maxCount = result[letter]
+            maxChar = letter;
+        }
+    });
+    return maxChar;
+}
+
+// find the maxcount char
+console.log(mostFrequentChar("javascript"));
+console.log(mostFrequentChar("hello worldmmmmmmmmmmmmmmmmmmm"));
+console.log(mostFrequentChar("aabbcc"));
+// Problem: Check if Two Strings Are Anagrams
+console.log("Problem: Check if Two Strings Are Anagrams")
+
+function StrAnagrams(str1, str2) {
+    const letter1 = str1.toLowerCase().split("").sort().join("");
+    const letter2 = str2.toLowerCase().split("").sort().join("");
+    return letter1 === letter2;
+}
+console.log(StrAnagrams("listen", "silent"))
+console.log(StrAnagrams("triangle", "integral"))
+console.log(StrAnagrams("hello", "world"))
